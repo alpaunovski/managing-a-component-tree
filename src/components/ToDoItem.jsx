c948ab7094d5b672c4ae36c3aac8f22cf2f23ed1
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-02-15 08:36:50 +0200

    update

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 function ToDoItem(props) {
 
-    //Keep track of the ToDoItem text decoration. Initialize to none, because items start as incomplete.
-
     return (
     <div onClick={() => {props.onChecked(props.id)}}><li>{props.text}</li></div> );
 }
